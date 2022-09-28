2b11151a03f3e49e69699b806fa788df1c68640d
Mahmudul-hasan16 <m.hasan5979@gmail.com>
2022-09-29 01:52:35 +0600

    first

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from '../../images/logo-01-01.png';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            <img src={logo} alt="" />
+            <div>
+                <a href="/instructor">Instructor</a>
+                <a href="/schedule">Schedule</a>
+                <a href="/blogs">Blogs</a>
+                <a href="/aboutus">About Us</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
